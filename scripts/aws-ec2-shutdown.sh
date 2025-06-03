9df1682162089620c2e837b56aba90471b90a485
mbcladwell <mbcladwell@gmail.com>
2025-06-03 05:52:18 -0400

    autocommit

--- scripts/aws-ec2-shutdown.sh
+++ scripts/aws-ec2-shutdown.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 export LC_ALL="C"
-aws ec2 stop-instances --instance-ids i-014db74d5072c5399 --region us-east-2 --profile default
+aws ec2 stop-instances --instance-ids i-01f4650cf64ee0e2d --region us-east-2 --profile default
